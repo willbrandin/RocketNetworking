d1ba3fd8ad1429ec0a86439566b60b59cceaa9d9
willbrandin <will@tidelaundry.com>
2019-08-29 15:49:23 -0500

    Watch target

---
+++ RocketWatch/RocketWatch.h
@@ -0,0 +1,19 @@
+//
+//  RocketWatch.h
+//  RocketWatch
+//
+//  Created by Will Brandin on 8/29/19.
+//  Copyright © 2019 williambrandin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RocketWatch.
+FOUNDATION_EXPORT double RocketWatchVersionNumber;
+
+//! Project version string for RocketWatch.
+FOUNDATION_EXPORT const unsigned char RocketWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RocketWatch/PublicHeader.h>
+
+
